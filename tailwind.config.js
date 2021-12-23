68a4308e0cd4d31d56bb9f7e8724bd9ea7080520
juancortelezzi <juanbicortelezzi@gmail.com>
2021-12-22 21:45:55 -0300

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bblack: "#030811",
+        bwhite: "#FEFEFE",
+        bgreen: "#38A169",
+      },
+    },
+  },
+  plugins: [],
+};
