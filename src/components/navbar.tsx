fe013227305997ceb88c25dbf54d7667bb89413f
juancortelezzi <juanbicortelezzi@gmail.com>
2021-12-22 21:46:11 -0300

    moved to src

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar: React.FC = () => {
+  return (
+    <div className="flex justify-between">
+      <a>J.B.C</a>
+      <div>
+        <a>Blog</a>
+        <a>Projects</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
